2cd6247b1753f31bad7cca694e7d83eae07fae20
Алексей <sayhell042@gmail.com>
2025-07-27 23:41:03 +0700

    Initial commit

---
+++ src/main/java/ru/top/pass_system/PassSystemApplication.java
@@ -0,0 +1,13 @@
+package ru.top.pass_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PassSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/top/pass_system/PassSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.top.pass_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
