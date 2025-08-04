a1c42a0a15a1fb6574ced092678c7e9bf1fcb0b7
EpicName56 <vladmont96@yandex.ru>
2025-08-04 15:59:06 +0500

    FEAT : (PAS-16) Pagination + specification (API CRITERIA) for Pass

--- src/main/java/ru/top/pass_system/controller/PassController.java
+++ src/main/java/ru/top/pass_system/controller/PassController.java
@@ -13,8 +13,6 @@ import ru.top.pass_system.dto.passDTO.PassResponseDTO;
 import ru.top.pass_system.dto.passDTO.PassUpdateDTO;
 import ru.top.pass_system.service.PassService;
 
-import java.net.URI;
-
 @RestController
 @RequestMapping("/api/pass")
 @RequiredArgsConstructor
@@ -22,45 +20,30 @@ public class PassController {
 
     private final PassService passService;
 
-    /**
-     * Создание нового пропуска
-     */
     @PostMapping
     public ResponseEntity<PassResponseDTO> create(@RequestBody PassCreateDTO passCreateDTO) {
-        PassResponseDTO createdPass = passService.create(passCreateDTO);
-        URI location = URI.create("/api/pass/" + createdPass.getId());
-        return ResponseEntity.created(location).body(createdPass);
+        return ResponseEntity.ok(passService.create(passCreateDTO));
     }
 
-    /**
-     * Получение списка пропусков с поддержкой фильтрации, сортировки и постраничного вывода
-     */
     @GetMapping
-    public ResponseEntity<Page<PassResponseDTO>> findAll(@ModelAttribute PassFilterDTO filter,
-                                                         @PageableDefault(size = 5, sort = {"id"}, direction = Sort.Direction.ASC) Pageable pageable) {
+    public ResponseEntity<Page<PassResponseDTO>> findAll(
+            @ModelAttribute PassFilterDTO filter,
+            @PageableDefault(size = 5, sort = "id", direction = Sort.Direction.ASC) Pageable pageable) {
+
         return ResponseEntity.ok(passService.findAll(filter, pageable));
     }
 
-    /**
-     * Получение одного пропуска по ID
-     */
-    @GetMapping("{id}")
+    @GetMapping(path = "{id}")
     public ResponseEntity<PassResponseDTO> findById(@PathVariable Long id) {
         return ResponseEntity.ok(passService.findById(id));
     }
 
-    /**
-     * Обновление существующего пропуска
-     */
     @PutMapping
     public ResponseEntity<PassResponseDTO> update(@RequestBody PassUpdateDTO passUpdateDTO) {
         return ResponseEntity.ok(passService.update(passUpdateDTO));
     }
 
-    /**
-     * Удаление пропуска по ID
-     */
-    @DeleteMapping("{id}")
+    @DeleteMapping(path = "{id}")
     public ResponseEntity<String> delete(@PathVariable Long id) {
         passService.delete(id);
         return ResponseEntity.ok("Пропуск с id: " + id + " успешно удалён.");
