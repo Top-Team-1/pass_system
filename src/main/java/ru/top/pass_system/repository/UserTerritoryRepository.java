914d4420ba96ccba6535422a7826ca7f4db54b7c
Алексей <sayhell042@gmail.com>
2025-08-10 10:15:40 +0700

    FEAT : (PAS-29 ) final redaction

--- src/main/java/ru/top/pass_system/repository/UserTerritoryRepository.java
+++ src/main/java/ru/top/pass_system/repository/UserTerritoryRepository.java
@@ -1,6 +1,8 @@
 package ru.top.pass_system.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.top.pass_system.model.UserTerritory;
 import ru.top.pass_system.model.UserTerritoryId;
 
@@ -9,11 +11,12 @@ import java.util.List;
 public interface UserTerritoryRepository extends JpaRepository<UserTerritory, UserTerritoryId> {
 
     // Найти все назначения по пользователю
-    List<UserTerritory> findByUser_Id(Long userId);
+//    List<UserTerritory> findByUser_Id(Long userId);
 
     // Найти все назначения по территории
-    List<UserTerritory> findByTerritory_Id(Long territoryId);
+//    List<UserTerritory> findByTerritory_Id(Long territoryId);
 
     // Проверить, существует ли связь
-    boolean existsByUser_IdAndTerritory_Id(Long userId, Long territoryId);
+    @Query(value = "SELECT EXISTS (SELECT 1 FROM user_territory WHERE user_id = :userId AND territory_id = :territoryId)", nativeQuery = true)
+    boolean existsByUserIdAndTerritoryId(@Param("userId") Long userId, @Param("territoryId") Long territoryId);
 }
